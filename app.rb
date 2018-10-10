37e96c91664dd937924a4014a03f18b2b539bc61
Adam Barton <adam.barton333@gmail.com>
2018-10-10 04:31:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,5 +5,13 @@ class App < Sinatra::Base
   get '/name/'do
     "My name is Adam"
   end
-  
+
+  get '/hometown/'do
+    "My hometown is Clearfield"
+  end
+
+  get '/favorite-song/'do
+    "My favorite song is 'Puff the Magic Dragon'".
+  end
+
 end
